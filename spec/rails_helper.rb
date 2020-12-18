b3c6f4ff326811e4782c6cfd5e907167c3001b41
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2020-12-18 10:48:23 +0200

    #35 Pundit rspec helper (#37)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require "rspec/rails"
-require "pundit/rspec"
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
