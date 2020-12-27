9eba0ed3efb13099dcd6aba3d4d7898c0a23b2df
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2020-12-28 01:52:02 +0200

    #98 Move formats endpoints to api namespace (#106)

--- spec/requests/api/formats_spec.rb
+++ spec/requests/api/formats_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
-RSpec.describe FormatsController do
+RSpec.describe Api::FormatsController do
   describe ".index" do
     it "returns valid response" do
       first_format = create(:format, tag: create(:tag, title: "A"))
       second_format = create(:format, tag: create(:tag, title: "B"))
       third_format = create(:format, tag: create(:tag, title: "C"))
 
-      get "/formats.json"
+      get "/api/formats.json"
 
       expect(response).to have_http_status(200)
-      expect(response).to match_json_schema("controllers/formats_controller/index/200")
+      expect(response).to match_json_schema("controllers/api/formats_controller/index/200")
 
       # Ensure that formats sorted by tag title ASC
       expect(response).to have_objects_collection(first_format, second_format, third_format)
@@ -21,10 +21,10 @@ RSpec.describe FormatsController do
     it "returns valid response" do
       format = create(:format)
 
-      get "/formats/#{format.to_param}.json"
+      get "/api/formats/#{format.to_param}.json"
 
       expect(response).to have_http_status(200)
-      expect(response).to match_json_schema("controllers/formats_controller/show/200")
+      expect(response).to match_json_schema("controllers/api/formats_controller/show/200")
     end
   end
 end
