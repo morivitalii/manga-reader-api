0c51f1e9d0760c44b5b2559515d166dc319c789c
Vitaly Mori <dev1.vitaly@gmail.com>
2020-12-27 11:34:48 +0200

    #88 Add formats to title

--- app/models/title.rb
+++ app/models/title.rb
@@ -4,6 +4,7 @@ class Title < ApplicationRecord
   has_many :resource_tags, as: :resource, dependent: :destroy
   has_many :tags, through: :resource_tags
   has_many :genres, through: :tags
+  has_many :formats, through: :tags
 
   translates :title
 
