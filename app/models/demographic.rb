f42a21cb371d96a75860303025c663de42213ea6
Vitaly Mori <dev1.vitaly@gmail.com>
2020-12-28 03:42:39 +0200

    Set tag_id as read only attribute in demographic model

--- app/models/demographic.rb
+++ app/models/demographic.rb
@@ -1,4 +1,7 @@
 class Demographic < ApplicationRecord
+  # Tag association should not be changed because it will create mess with content tagging
+  attr_readonly :tag_id
+
   belongs_to :tag
 
   has_many :titles, through: :tag
