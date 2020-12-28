e44f59cd02b95ed1c29b21805e957d9fa8de4ce6
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2020-12-28 02:15:00 +0200

    Set key as read only attribute in locale model (#110)

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -1,6 +1,9 @@
 class Locale < ApplicationRecord
+  # Key attribute should not be changed because a lot of things depends on it and changes will create chaos everywhere
+  attr_readonly :key
+
   has_many :content_languages, dependent: :restrict_with_error
-  has_many :interface_languages    , dependent: :restrict_with_error
+  has_many :interface_languages, dependent: :restrict_with_error
 
   validates :key, presence: true, uniqueness: { case_sensitive: false }
 end
