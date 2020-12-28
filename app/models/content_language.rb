13c76264f09b3279f7dcafeb24e34d79acb911ae
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2020-12-28 02:49:34 +0200

    Set locale_id as read only attribute in content language model (#112)

--- app/models/content_language.rb
+++ app/models/content_language.rb
@@ -1,6 +1,9 @@
 class ContentLanguage < ApplicationRecord
   include Translation
 
+  # Locale association should not be changed because it and will create mess with translations
+  attr_readonly :locale_id
+
   belongs_to :locale
 
   translates :title
