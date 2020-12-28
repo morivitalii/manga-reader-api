729bf219290c44d6d28be0e95f971a5c56cd9d45
Vitaly Mori <dev1.vitaly@gmail.com>
2020-12-28 02:43:23 +0200

    Set locale_id as read only attribute in interface language model

--- app/models/interface_language.rb
+++ app/models/interface_language.rb
@@ -1,6 +1,9 @@
 class InterfaceLanguage < ApplicationRecord
   include Translation
 
+  # Locale attribute should not be changed because it and will create confusions in user interfaces
+  attr_readonly :locale_id
+
   belongs_to :locale
 
   translates :title
