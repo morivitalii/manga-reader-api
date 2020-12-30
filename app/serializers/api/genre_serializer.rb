0ee1fbe808629f8f216c19ac71b943f5eb7bcd55
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2020-12-30 08:03:26 +0200

    #124 Remove translations from genres serializer (#129)

--- app/serializers/api/genre_serializer.rb
+++ app/serializers/api/genre_serializer.rb
@@ -5,17 +5,7 @@ class Api::GenreSerializer < Api::ApplicationSerializer
       tag_id: model.tag_id,
       title: model.tag.title,
       created_at: model.created_at,
-      updated_at: model.updated_at,
-      translations: tag.attributes[:translations],
+      updated_at: model.updated_at
     }
-  end
-
-  private
-
-  def tag
-    return nil unless model.association(:tag).loaded?
-    return nil if model.tag.blank?
-
-    Api::TagSerializer.serialize(model.tag)
   end
 end
