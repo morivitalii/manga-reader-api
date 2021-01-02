8ed917e2a7b83503660c37ac00628a59dd9d65b0
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-02 10:53:56 +0200

    Api::ContentLanguagesController#show authorization

--- app/policies/api/content_languages_policy.rb
+++ app/policies/api/content_languages_policy.rb
@@ -2,4 +2,8 @@ class Api::ContentLanguagesPolicy < Api::ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    true
+  end
 end
--- spec/policies/api/content_languages_policy_spec.rb
+++ spec/policies/api/content_languages_policy_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Api::ContentLanguagesPolicy do
   permissions :index? do
     it { is_expected.to permit }
   end
+
+  permissions :show? do
+    let(:content_language) { create(:content_language) }
+
+    it { is_expected.to permit(nil, content_language) }
+  end
 end
