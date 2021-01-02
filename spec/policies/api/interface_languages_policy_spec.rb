029aa2c744b5a9fbf6d70a55466bf4f2bd260088
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-02 11:02:57 +0200

    Api::InterfaceLanguagesController#show authorization (#165)

--- spec/policies/api/interface_languages_policy_spec.rb
+++ spec/policies/api/interface_languages_policy_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Api::InterfaceLanguagesPolicy do
   permissions :index? do
     it { is_expected.to permit }
   end
+
+  permissions :show? do
+    let(:interface_language) { create(:interface_language) }
+
+    it { is_expected.to permit(nil, interface_language) }
+  end
 end
