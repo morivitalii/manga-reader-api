532e4fa8d8f784073814cff17dbe8ea75169d603
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-02 10:43:49 +0200

    Api::ThemesController#show authorization

--- app/policies/api/themes_policy.rb
+++ app/policies/api/themes_policy.rb
@@ -2,4 +2,8 @@ class Api::ThemesPolicy < Api::ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    true
+  end
 end
--- spec/policies/api/themes_policy_spec.rb
+++ spec/policies/api/themes_policy_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Api::ThemesPolicy do
   permissions :index? do
     it { is_expected.to permit }
   end
+
+  permissions :show? do
+    let(:theme) { create(:theme) }
+
+    it { is_expected.to permit(nil, theme) }
+  end
 end
