59739f747f342c6a5a392465a2c8f94e3f5d2941
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-05 23:46:10 +0200

    Add tag_id, resource_type, resource_id as read only attributes to resource tag (#212)

--- app/models/resource_tag.rb
+++ app/models/resource_tag.rb
@@ -1,4 +1,7 @@
 class ResourceTag < ApplicationRecord
+  # This attributes should not be changed. Just because
+  attr_readonly :tag_id, :resource_type, :resource_id
+
   belongs_to :tag
   belongs_to :resource, polymorphic: true
 
