5601eba8ba24b71d922bfac5c5fb13fa617d074a
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-07 15:45:51 +0200

    User endpoint (#214)

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,10 @@
 class Api::UsersController < Api::ApplicationController
   include Pagination
 
+  before_action :set_user, only: [:show]
+
   before_action -> { authorize(Api::UsersPolicy) }, only: [:index]
+  before_action -> { authorize(Api::UsersPolicy, @user) }, only: [:show]
 
   def index
     users = User.order(username: :asc).all
@@ -13,5 +16,18 @@ class Api::UsersController < Api::ApplicationController
     set_pagination_headers(pagination)
 
     render json: users, status: 200
+  end
+
+  def show
+    user = Api::UserDecorator.decorate(@user)
+    user = Api::UserSerializer.serialize(user)
+
+    render json: user, status: 200
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
