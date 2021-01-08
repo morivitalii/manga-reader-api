883005e8b9bdbf0437265621aed479a432927243
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-08 23:26:04 +0200

    Fix group user decorator class name (#220)

--- app/decorators/api/group_user_decorator.rb
+++ app/decorators/api/group_user_decorator.rb
@@ -1,4 +1,4 @@
-class Api::GroupDecorator < Api::ApplicationDecorator
+class Api::GroupUserDecorator < Api::ApplicationDecorator
   decorates_association :group, with: Api::GroupDecorator
   decorates_association :user, with: Api::UserDecorator
 end
