a9677c26a3c04d36e5c9713b93b2164412961bc6
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-09 17:07:09 +0200

    Api::TagsPolicy spec contexts

--- spec/policies/api/tags_policy_spec.rb
+++ spec/policies/api/tags_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::TagsPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:tag) { create(:tag) }
+
+      it { is_expected.to permit(current_user, tag) }
+    end
   end
 
-  permissions :show? do
-    let(:tag) { create(:tag) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, tag) }
+    permissions :show? do
+      let(:tag) { create(:tag) }
+
+      it { is_expected.to permit(current_user, tag) }
+    end
   end
 end
