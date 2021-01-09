245e411b994623ec68095b5027523e768c55699b
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-09 02:09:40 +0200

    Policy scope for database queries in Api::Tags::TranslationsController (#222)

--- app/policies/api/tags/translations_policy.rb
+++ app/policies/api/tags/translations_policy.rb
@@ -2,4 +2,10 @@ class Api::Tags::TranslationsPolicy < Api::ApplicationPolicy
   def show?
     true
   end
+
+  class Scope < Api::ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
 end
