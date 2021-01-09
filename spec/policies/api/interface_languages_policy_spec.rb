534eea890cff11d3b522f1eb401ffeb21394c7c9
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-09 17:09:56 +0200

    Api::InterfaceLanguagesPolicy spec contexts

--- spec/policies/api/interface_languages_policy_spec.rb
+++ spec/policies/api/interface_languages_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::InterfaceLanguagesPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:interface_language) { create(:interface_language) }
+
+      it { is_expected.to permit(current_user, interface_language) }
+    end
   end
 
-  permissions :show? do
-    let(:interface_language) { create(:interface_language) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, interface_language) }
+    permissions :show? do
+      let(:interface_language) { create(:interface_language) }
+
+      it { is_expected.to permit(current_user, interface_language) }
+    end
   end
 end
