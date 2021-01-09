58be537889c96f864fb85491ca1e6c7484b4c61f
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-09 17:19:15 +0200

    Api::Tags::TranslationsPolicy spec contexts

--- spec/policies/api/tags/translations_policy_spec.rb
+++ spec/policies/api/tags/translations_policy_spec.rb
@@ -3,9 +3,19 @@ require "rails_helper"
 RSpec.describe Api::Tags::TranslationsPolicy do
   subject { described_class }
 
-  permissions :show? do
-    let(:tag) { create(:tag) }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :show? do
+      let(:tag) { create(:tag) }
 
-    it { is_expected.to permit(nil, tag) }
+      it { is_expected.to permit(current_user, tag) }
+    end
+  end
+
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :show? do
+      let(:tag) { create(:tag) }
+
+      it { is_expected.to permit(current_user, tag) }
+    end
   end
 end
