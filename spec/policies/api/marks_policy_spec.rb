330917433f902a2b338775d112dff8ba31d59be4
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-09 17:08:48 +0200

    Api::MarksPolicy spec contexts (#239)

--- spec/policies/api/marks_policy_spec.rb
+++ spec/policies/api/marks_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::MarksPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:mark) { create(:mark) }
+
+      it { is_expected.to permit(current_user, mark) }
+    end
   end
 
-  permissions :show? do
-    let(:mark) { create(:mark) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, mark) }
+    permissions :show? do
+      let(:mark) { create(:mark) }
+
+      it { is_expected.to permit(current_user, mark) }
+    end
   end
 end
