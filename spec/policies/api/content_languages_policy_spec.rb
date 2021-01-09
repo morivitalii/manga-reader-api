cac27e7ac43cddec828a57b8fcc51f24a7b6586f
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-09 17:17:19 +0200

    Api::ContentLanguagesPolicy spec contexts

--- spec/policies/api/content_languages_policy_spec.rb
+++ spec/policies/api/content_languages_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::ContentLanguagesPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:content_language) { create(:content_language) }
+
+      it { is_expected.to permit(current_user, content_language) }
+    end
   end
 
-  permissions :show? do
-    let(:content_language) { create(:content_language) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, content_language) }
+    permissions :show? do
+      let(:content_language) { create(:content_language) }
+
+      it { is_expected.to permit(current_user, content_language) }
+    end
   end
 end
