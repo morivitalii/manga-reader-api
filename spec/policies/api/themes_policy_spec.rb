5e8f84e4d834cf925f6b154408493a7d90cd9901
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-09 17:06:09 +0200

    Api::ThemesPolicy spec contexts (#237)

--- spec/policies/api/themes_policy_spec.rb
+++ spec/policies/api/themes_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::ThemesPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:theme) { create(:theme) }
+
+      it { is_expected.to permit(current_user, theme) }
+    end
   end
 
-  permissions :show? do
-    let(:theme) { create(:theme) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, theme) }
+    permissions :show? do
+      let(:theme) { create(:theme) }
+
+      it { is_expected.to permit(current_user, theme) }
+    end
   end
 end
