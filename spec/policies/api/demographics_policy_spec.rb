9d77a6fa660eb9d38462b3e3f47d70b2df7250e2
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-09 17:15:36 +0200

    Api::DemographicsPolicy spec contexts (#243)

--- spec/policies/api/demographics_policy_spec.rb
+++ spec/policies/api/demographics_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::DemographicsPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:demographic) { create(:demographic) }
+
+      it { is_expected.to permit(current_user, demographic) }
+    end
   end
 
-  permissions :show? do
-    let(:demographic) { create(:demographic) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, demographic) }
+    permissions :show? do
+      let(:demographic) { create(:demographic) }
+
+      it { is_expected.to permit(current_user, demographic) }
+    end
   end
 end
