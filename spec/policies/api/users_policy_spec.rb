f41e6a8c60f936eb92aa416ee3db3904779dda89
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-09 03:06:10 +0200

    Api::UsersPolicy spec contexts

--- spec/policies/api/users_policy_spec.rb
+++ spec/policies/api/users_policy_spec.rb
@@ -3,13 +3,27 @@ require "rails_helper"
 RSpec.describe Api::UsersPolicy do
   subject { described_class }
 
-  permissions :index? do
-    it { is_expected.to permit }
+  context "as signed out user", context: :as_signed_out_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
+
+    permissions :show? do
+      let(:user) { create(:user) }
+
+      it { is_expected.to permit(current_user, user) }
+    end
   end
 
-  permissions :show? do
-    let(:user) { create(:user) }
+  context "as signed in user", context: :as_signed_in_user do
+    permissions :index? do
+      it { is_expected.to permit(current_user) }
+    end
 
-    it { is_expected.to permit(nil, user) }
+    permissions :show? do
+      let(:user) { create(:user) }
+
+      it { is_expected.to permit(current_user, user) }
+    end
   end
 end
