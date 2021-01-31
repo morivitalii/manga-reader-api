c2ae6fdeadc287a556f164dc78d96be9084d760c
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-01-31 19:21:11 +0200

    Fix AssociationTypeValidator (#263)

--- app/validators/association_type_validator.rb
+++ app/validators/association_type_validator.rb
@@ -1,6 +1,6 @@
 class AssociationTypeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.in?(options[:in])
+    unless value.class.name.in?(options[:in])
       record.errors.add(attribute, :inclusion)
     end
   end
