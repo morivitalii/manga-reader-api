ad4515e6b8d10732b066c3cdac1aecc0731f1ca0
Vitaly Mori <dev1.vitaly@gmail.com>
2021-01-31 18:21:45 +0200

    ResourceFormat resource type validation

--- app/models/resource_format.rb
+++ app/models/resource_format.rb
@@ -6,4 +6,5 @@ class ResourceFormat < ApplicationRecord
   belongs_to :resource, polymorphic: true
 
   validates :format, presence: true, uniqueness: {scope: [:resource_type, :resource_id]}
+  validates :resource, association_type: { in: ["Title"] }
 end
