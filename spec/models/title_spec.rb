aa6c369677461ebb5b4adf7e597a9d581df9ac03
Vitaly Mori <dev1.vitaly@gmail.com>
2021-02-06 17:59:21 +0200

    Add length validation on Title.title

--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Title do
     subject { create(:title) }
 
     it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_length_of(:title).is_at_least(1).is_at_most(125) }
   end
 end
