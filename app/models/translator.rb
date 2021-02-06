647b23ccbb18a76f6535b5ed552c00412190be8e
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-02-06 16:32:30 +0200

    Fix artists models pages association defilinition (#300)

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -5,7 +5,7 @@ class Translator < ApplicationRecord
   belongs_to :artist
 
   has_many :resource_translators, dependent: :restrict_with_error
-  has_many :pages, dependent: :restrict_with_error
+  has_many :pages, through: :resource_translators, source: :resource, source_type: "Title"
 
   validates :artist, uniqueness: true
 end
