649091b6fccc58611069c6444b2671e571008c04
Vitaly Mori <dev1.vitaly@gmail.com>
2021-02-10 18:00:46 +0200

    Title volumes endpoints

---
+++ app/controllers/api/titles/volumes_controller.rb
@@ -0,0 +1,41 @@
+class Api::Titles::VolumesController < Api::ApplicationController
+  before_action :set_title, only: [:index, :show]
+  before_action :set_volume, only: [:show]
+
+  before_action -> { authorize(Api::Titles::VolumesPolicy) }, only: [:index]
+  before_action -> { authorize(Api::Titles::VolumesPolicy, @volume) }, only: [:show]
+
+  def index
+    volumes = volumes_scope.order("volumes.number ASC").all
+
+    volumes = Api::VolumeDecorator.decorate(volumes)
+    volumes = Api::VolumeSerializer.serialize(volumes)
+
+    render json: volumes, status: 200
+  end
+
+  def show
+    volume = Api::VolumeDecorator.decorate(@volume)
+    volume = Api::VolumeSerializer.serialize(volume)
+
+    render json: volume, status: 200
+  end
+
+  private
+
+  def set_title
+    @title = titles_scope.find(params[:title_id])
+  end
+
+  def set_volume
+    @volume = volumes_scope.find(params[:id])
+  end
+  
+  def titles_scope
+    policy_scope(Api::TitlesPolicy, Title)
+  end
+
+  def volumes_scope
+    policy_scope(Api::Titles::VolumesPolicy, @title.volumes)
+  end
+end
