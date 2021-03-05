22f841b6bb74cbe867af1937f8542c43699932b7
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-03-06 00:02:00 +0200

    Add bookmarks count to user (#348)

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   # This attributes should not be changed. Just because
   attr_readonly :user_id, :resource_type, :resource_id, :created_at, :updated_at
 
-  belongs_to :user
+  belongs_to :user, counter_cache: :bookmarks_count
   belongs_to :resource, polymorphic: true, counter_cache: :bookmarks_count
 
   validates :user, presence: true, uniqueness: {scope: [:resource_type, :resource_id]}
