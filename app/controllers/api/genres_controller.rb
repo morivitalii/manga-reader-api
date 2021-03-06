b65865a07c7948c0a974131932b7f6acba469d53
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-03-06 22:13:37 +0200

    Genres endpoints cache (#354)

--- app/controllers/api/genres_controller.rb
+++ app/controllers/api/genres_controller.rb
@@ -1,26 +1,40 @@
 class Api::GenresController < Api::ApplicationController
   before_action :set_genre, only: [:show]
-  before_action :set_genre_associations, only: [:show]
 
   before_action -> { authorize(Api::GenresPolicy) }, only: [:index]
   before_action -> { authorize(Api::GenresPolicy, @genre) }, only: [:show]
 
   def index
-    genres = genres_scope.joins(tag: :translations).order("tag_translations.title ASC").all
+    query = genres_scope.joins(tag: :translations).order("tag_translations.title ASC")
+    cache_key = cache_key(query)
 
-    ActiveRecord::Associations::Preloader.new.preload(
-      genres, :tag, Tag.with_translations
-    )
+    genres = Rails.cache.fetch(cache_key) do
+      genres = query.all
 
-    genres = Api::GenreDecorator.decorate_collection(genres)
-    genres = Api::GenreSerializer.serialize(genres)
+      ActiveRecord::Associations::Preloader.new.preload(
+        genres, :tag, Tag.with_translations
+      )
+
+      genres = Api::GenreDecorator.decorate_collection(genres)
+
+      Api::GenreSerializer.serialize(genres).to_json
+    end
 
     render json: genres, status: 200
   end
 
   def show
-    genre = Api::GenreDecorator.decorate(@genre)
-    genre = Api::GenreSerializer.serialize(genre)
+    cache_key = cache_key(@genre)
+
+    genre = Rails.cache.fetch(cache_key) do
+      ActiveRecord::Associations::Preloader.new.preload(
+        @genre, :tag, Tag.with_translations
+      )
+
+      genre = Api::GenreDecorator.decorate(@genre)
+
+      Api::GenreSerializer.serialize(genre).to_json
+    end
 
     render json: genre, status: 200
   end
@@ -33,11 +47,5 @@ class Api::GenresController < Api::ApplicationController
 
   def genres_scope
     policy_scope(Api::GenresPolicy, Genre)
-  end
-
-  def set_genre_associations
-    ActiveRecord::Associations::Preloader.new.preload(
-      @genre, :tag, Tag.with_translations
-    )
   end
 end
