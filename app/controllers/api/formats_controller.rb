d09ef31d70654551d9b3af62e327d0af0304eea1
Vitaly Mori <dev1.vitaly@gmail.com>
2021-03-06 22:23:18 +0200

    Formats endpoints cache

--- app/controllers/api/formats_controller.rb
+++ app/controllers/api/formats_controller.rb
@@ -1,26 +1,40 @@
 class Api::FormatsController < Api::ApplicationController
   before_action :set_format, only: [:show]
-  before_action :set_format_associations, only: [:show]
 
   before_action -> { authorize(Api::FormatsPolicy) }, only: [:index]
   before_action -> { authorize(Api::FormatsPolicy, @format) }, only: [:show]
 
   def index
-    formats = format_scope.joins(tag: :translations).order("tag_translations.title ASC").all
+    query = format_scope.joins(tag: :translations).order("tag_translations.title ASC")
+    cache_key = cache_key(query)
 
-    ActiveRecord::Associations::Preloader.new.preload(
-      formats, :tag, Tag.with_translations
-    )
+    formats = Rails.cache.fetch(cache_key) do
+      formats = query.all
 
-    formats = Api::FormatDecorator.decorate_collection(formats)
-    formats = Api::FormatSerializer.serialize(formats)
+      ActiveRecord::Associations::Preloader.new.preload(
+        formats, :tag, Tag.with_translations
+      )
+
+      formats = Api::FormatDecorator.decorate_collection(formats)
+      
+      Api::FormatSerializer.serialize(formats).to_json
+    end
 
     render json: formats, status: 200
   end
 
   def show
-    format = Api::FormatDecorator.decorate(@format)
-    format = Api::FormatSerializer.serialize(format)
+    cache_key = cache_key(@format)
+
+    format = Rails.cache.fetch(cache_key) do
+      ActiveRecord::Associations::Preloader.new.preload(
+        @format, :tag, Tag.with_translations
+      )
+
+      format = Api::FormatDecorator.decorate(@format)
+
+      Api::FormatSerializer.serialize(format).to_json
+    end
 
     render json: format, status: 200
   end
@@ -33,11 +47,5 @@ class Api::FormatsController < Api::ApplicationController
 
   def format_scope
     policy_scope(Api::FormatsPolicy, Format)
-  end
-
-  def set_format_associations
-    ActiveRecord::Associations::Preloader.new.preload(
-      @format, :tag, Tag.with_translations
-    )
   end
 end
