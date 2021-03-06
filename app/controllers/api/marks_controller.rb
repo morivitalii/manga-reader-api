2c94866a4522643a9cde33ed709d0cd36944a0ac
Vitaly Mori <dev1.vitaly@gmail.com>
2021-03-06 22:28:26 +0200

    Marks endpoints cache

--- app/controllers/api/marks_controller.rb
+++ app/controllers/api/marks_controller.rb
@@ -1,26 +1,40 @@
 class Api::MarksController < Api::ApplicationController
   before_action :set_mark, only: [:show]
-  before_action :set_mark_associations, only: [:show]
 
   before_action -> { authorize(Api::MarksPolicy) }, only: [:index]
   before_action -> { authorize(Api::MarksPolicy, @mark) }, only: [:show]
 
   def index
-    marks = marks_scope.joins(tag: :translations).order("tag_translations.title ASC").all
+    query = marks_scope.joins(tag: :translations).order("tag_translations.title ASC")
+    cache_key = cache_key(query)
 
-    ActiveRecord::Associations::Preloader.new.preload(
-      marks, :tag, Tag.with_translations
-    )
+    marks = Rails.cache.fetch(cache_key) do
+      marks = query.all
 
-    marks = Api::MarkDecorator.decorate_collection(marks)
-    marks = Api::MarkSerializer.serialize(marks)
+      ActiveRecord::Associations::Preloader.new.preload(
+        marks, :tag, Tag.with_translations
+      )
+
+      marks = Api::MarkDecorator.decorate_collection(marks)
+
+      Api::MarkSerializer.serialize(marks).to_json
+    end
 
     render json: marks, status: 200
   end
 
   def show
-    mark = Api::MarkDecorator.decorate(@mark)
-    mark = Api::MarkSerializer.serialize(mark)
+    cache_key = cache_key(@mark)
+
+    mark = Rails.cache.fetch(cache_key) do
+      ActiveRecord::Associations::Preloader.new.preload(
+        @mark, :tag, Tag.with_translations
+      )
+
+      mark = Api::MarkDecorator.decorate(@mark)
+
+      Api::MarkSerializer.serialize(mark).to_json
+    end
 
     render json: mark, status: 200
   end
@@ -33,11 +47,5 @@ class Api::MarksController < Api::ApplicationController
 
   def marks_scope
     policy_scope(Api::MarksPolicy, Mark)
-  end
-
-  def set_mark_associations
-    ActiveRecord::Associations::Preloader.new.preload(
-      @mark, :tag, Tag.with_translations
-    )
   end
 end
