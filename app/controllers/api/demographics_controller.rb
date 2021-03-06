75079bcdc5b75823d7261f16f4f2506eeee2b020
Vitaly Mori <dev1.vitaly@gmail.com>
2021-03-06 22:18:02 +0200

    Demographics endpoints cache

--- app/controllers/api/demographics_controller.rb
+++ app/controllers/api/demographics_controller.rb
@@ -1,26 +1,39 @@
 class Api::DemographicsController < Api::ApplicationController
   before_action :set_demographic, only: [:show]
-  before_action :set_demographic_associations, only: [:show]
 
   before_action -> { authorize(Api::DemographicsPolicy) }, only: [:index]
   before_action -> { authorize(Api::DemographicsPolicy, @demographic) }, only: [:show]
 
   def index
-    demographics = demographics_scope.joins(tag: :translations).order("tag_translations.title ASC").all
+    query = demographics_scope.joins(tag: :translations).order("tag_translations.title ASC")
+    cache_key = cache_key(query)
 
-    ActiveRecord::Associations::Preloader.new.preload(
-      demographics, :tag, Tag.with_translations
-    )
+    demographics = Rails.cache.fetch(cache_key) do
+      demographics = query.all
 
-    demographics = Api::DemographicDecorator.decorate_collection(demographics)
-    demographics = Api::DemographicSerializer.serialize(demographics)
+      ActiveRecord::Associations::Preloader.new.preload(
+        demographics, :tag, Tag.with_translations
+      )
+
+      demographics = Api::DemographicDecorator.decorate_collection(demographics)
+      Api::DemographicSerializer.serialize(demographics).to_json
+    end
 
     render json: demographics, status: 200
   end
 
   def show
-    demographic = Api::DemographicDecorator.decorate(@demographic)
-    demographic = Api::DemographicSerializer.serialize(demographic)
+    cache_key = cache_key(@demographic)
+
+    demographic = Rails.cache.fetch(cache_key) do
+      ActiveRecord::Associations::Preloader.new.preload(
+        @demographic, :tag, Tag.with_translations
+      )
+
+      demographic = Api::DemographicDecorator.decorate(@demographic)
+
+      Api::DemographicSerializer.serialize(demographic).to_json
+    end
 
     render json: demographic, status: 200
   end
@@ -33,11 +46,5 @@ class Api::DemographicsController < Api::ApplicationController
 
   def demographics_scope
     policy_scope(Api::DemographicsPolicy, Demographic)
-  end
-
-  def set_demographic_associations
-    ActiveRecord::Associations::Preloader.new.preload(
-      @demographic, :tag, Tag.with_translations
-    )
   end
 end
