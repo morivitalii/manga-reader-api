cc7e094b9e969c216897add81da60bd7e7b9e0bf
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-03-09 06:01:45 +0200

    Frontend (#367)
    * Vue 3
    
    * Bootstrap

---
+++ app/javascript/application.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div v-for="message in messages">
+      <p>{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      messages: ["Hello Vue!", "Fuck yeah!"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
