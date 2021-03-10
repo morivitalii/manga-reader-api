a96b11236f432aa8caf2325c0e6c519537086771
Vitaly Mori <dev1.vitaly@gmail.com>
2021-03-10 08:25:55 +0200

    Send chapter to published endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Rails.application.routes.draw do
           scope module: :chapters do
             resource :to_draft, only: [:update], controller: :to_draft
             resource :to_review, only: [:update], controller: :to_review
+            resource :to_published, only: [:update], controller: :to_published
             resource :cover, only: [:update, :destroy], controller: :cover
             resources :views, only: [:create]
             resources :bookmarks, only: [:create, :destroy]
