e422223efcb6888739daf6e935ff979c7faf15a0
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-03-24 16:15:32 +0200

    Add users_count to groups (#389)

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -4,7 +4,7 @@ class GroupUser < ApplicationRecord
   # This attributes should not be changed. Just because
   attr_readonly :group_id, :user_id
 
-  belongs_to :group
+  belongs_to :group, counter_cache: :users_count
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: [:group_id] }
