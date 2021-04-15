3a14dfd3657473ff9f1792cd3c3f7782a1b2cb1b
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-04-15 11:21:39 +0300

    Change to_review title endpoint response (#405)

--- spec/requests/api/titles/to_review_spec.rb
+++ spec/requests/api/titles/to_review_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Api::Titles::ToReviewController do
-  describe ".create", context: :as_signed_in_user do
+  describe ".update", context: :as_signed_in_user do
     it "returns valid response" do
       title = create(:draft_title)
 
       put "/api/titles/#{title.to_param}/to_review.json"
 
-      expect(response).to have_http_status(204)
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/titles/to_review_controller/update/200")
     end
   end
 end
