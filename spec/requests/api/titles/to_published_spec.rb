85c361813b3a2c12f66c10c0d9f6419bdd4178de
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-04-15 11:40:08 +0300

    Change to_published title endpoint response (#409)

--- spec/requests/api/titles/to_published_spec.rb
+++ spec/requests/api/titles/to_published_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Api::Titles::ToPublishedController do
 
       put "/api/titles/#{title.to_param}/to_published.json"
 
-      expect(response).to have_http_status(204)
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/titles/to_review_controller/update/200")
     end
   end
 end
