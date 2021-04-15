5ddbcf291cb6eb01cb02e9258885ae0aa5d81725
Vitaly Mori <dev1vitaly@users.noreply.github.com>
2021-04-15 10:56:15 +0300

    Change to_draft title endpoint response (#404)

--- spec/requests/api/titles/to_draft_spec.rb
+++ spec/requests/api/titles/to_draft_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Api::Titles::ToDraftController do
 
       put "/api/titles/#{title.to_param}/to_draft.json"
 
-      expect(response).to have_http_status(204)
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/titles/to_draft_controller/create/200")
     end
   end
 end
