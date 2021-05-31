18f8e343fc61d4598408ff89d4be7d4edf82cdd6
Mori Vitalii <6964435+morivitalii@users.noreply.github.com>
2021-05-31 16:48:16 +0300

    Add associations preloading to sign in and sign up controllers (#448)

--- app/controllers/api/sign_up_controller.rb
+++ app/controllers/api/sign_up_controller.rb
@@ -9,6 +9,15 @@ class Api::SignUpController < Api::ApplicationController
     if service.call
       request.env["warden"].set_user(service.user)
 
+      ActiveRecord::Associations::Preloader.new.preload(
+        service.user, [
+          :access_rights,
+          user_setting: {
+            avatar_attachment: :blob
+          }
+        ]
+      )
+
       user = Api::UserDecorator.decorate(service.user)
       user = Api::UserSerializer.serialize(user)
 
