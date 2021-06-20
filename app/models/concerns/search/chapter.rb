32ac7ac87e950caf0c39109ca42acbc52ad84c41
Mori Vitalii <6964435+morivitalii@users.noreply.github.com>
2021-06-20 15:02:12 +0300

    Chapter indexing (#470)

---
+++ app/models/concerns/search/chapter.rb
@@ -0,0 +1,55 @@
+module Search
+	module Chapter
+		extend ActiveSupport::Concern
+
+		included do
+			include Elasticsearch::Model
+
+			settings do
+				mappings dynamic: false, _source: { enabled: false } do
+					indexes :content_language_id, type: :long
+					indexes :title_id, type: :long
+					indexes :volume_id, type: :long
+					indexes :group_id, type: :long
+					indexes :number, type: :integer
+					indexes :status, type: :keyword
+					indexes :views_count, type: :long
+					indexes :bookmarks_count, type: :long
+					indexes :pages_count, type: :integer
+					indexes :name, type: :text
+					indexes :created_at, type: :date
+					indexes :updated_at, type: :date
+					indexes :deleted_at, type: :date
+					indexes :sent_to_review_at, type: :date
+					indexes :published_at, type: :date
+				end
+			end
+
+			def as_indexed_json(options = {})
+				ActiveRecord::Associations::Preloader.new.preload(
+						self, [
+						self.class.translations_associations
+					]
+				)
+
+				{
+					content_language_id: content_language_id,
+					title_id: title_id,
+					volume_id: volume_id,
+					group_id: group_id,
+					number: number,
+					status: status,
+					views_count: views_count,
+					bookmarks_count: bookmarks_count,
+					pages_count: pages_count,
+					name: name,
+					created_at: created_at,
+					updated_at: updated_at,
+					deleted_at: deleted_at,
+					sent_to_review_at: sent_to_review_at,
+					published_at: published_at,
+				}.to_json
+			end
+		end
+	end
+end
