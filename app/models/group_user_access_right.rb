10f8f5ff38652dc95aff9f22912dcef2927fb202
Mori Vitalii <mori.vitalii@gmail.com>
2021-07-11 20:32:11 +0300

    Add association cache invalidation to GroupUserAccessRight model

--- app/models/group_user_access_right.rb
+++ app/models/group_user_access_right.rb
@@ -1,9 +1,13 @@
 class GroupUserAccessRight < ApplicationRecord
+  include CacheInvalidation
+
   # This attributes should not be changed. Just because
   attr_readonly :group_access_right_id, :group_user_id
 
   belongs_to :group_access_right
   belongs_to :group_user
+
+  invalidate_association_cache :group_user
 
   validates :group_access_right_id, uniqueness: { scope: [:group_user_id] }
 end
